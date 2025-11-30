291db68e02ee45671691d33a4ad5378fdcdfa9e0
lois-rodier <lois.rodier2@gmail.com>
2025-11-30 22:03:23 +0100

    Create main_annotation_puzzle.py

---
+++ main_annotation_puzzle.py
@@ -0,0 +1,18 @@
+import numpy as np
+from donnee_puzzle import puzzle1
+from coins import coins
+
+
+puzzle = puzzle1
+pieces = puzzle[0]["predictions"]["predictions"]
+coins_listes = []
+for piece in pieces:
+    couples = piece["points"]
+    L = len(couples)
+    contour = np.empty((L, 2))
+    for k in range(L):
+        couple = couples[k]
+        contour[k, 0] = couple["x"]
+        contour[k, 1] = couple["y"]
+    coins_listes.append(coins(contour))
+print(coins_listes)
